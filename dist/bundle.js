0035c99b0958baf7d57d2d9c1975e1b943c51ca2
FrancescaDUffizi <francescaduffizi@hotmail.it>
2022-08-23 19:57:33 +0200

    ProjectBrowser - IfcClasses
    Select by IfcClass inside draggable div

--- dist/bundle.js
+++ dist/bundle.js
@@ -112793,11 +112793,6 @@ if (currentProjectID === "0") {
       currentProject = p;
     }
   }
-  console.log(projects.id);
-  console.log(
-    typeof currentProjectID + " vs " + " currentID= " + currentProjectID
-  );
-  console.log("currentProject= " + currentProject);
 
   const title = document.getElementById("project-title");
   title.innerHTML = currentProject.name;
@@ -112805,24 +112800,79 @@ if (currentProjectID === "0") {
   loadIfc(currentPath);
 }
 
+
+
 async function loadIfc(url) {
   await viewer.IFC.setWasmPath("../");
   const model = await viewer.IFC.loadIfcUrl(url);
-  // Add dropped shadow and post-processing efect
+  // Add dropped shadow and post-processing effect
   await viewer.shadowDropper.renderShadow(model.modelID);
   viewer.context.renderer.postProduction.active = true;
   const ifcProject = await viewer.IFC.getSpatialStructure(model.modelID);
   //console.log("Treeview= " + JSON.stringify(ifcProject));
   await createTreeMenu(ifcProject, model.modelID);
+  //const v= getIfcClass(ifcProject);
+  //console.log(v);
+  test(ifcProject, model.modelID);
 }
+
 window.ondblclick = async () => await viewer.IFC.selector.pickIfcItem();
 window.onmousemove = async () => await viewer.IFC.selector.prePickIfcItem();
+
+//Make the DIV element draggagle:
+dragElement(document.getElementById("mydiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
 
 const backBtn=document.getElementById("btn-back");
 backBtn.onclick=()=>{
   viewer.dispose();
   window.open("./index.html","_self");
 };
+
+
+
 //HIDE BUTTON ----------------------------------------------------------------------------------------
 async function hide() {
   window.ondblclick = async () => {
@@ -112885,11 +112935,92 @@ sectionButton.onclick=()=>{
     viewer.clipper.createPlane();
   };
   window.onkeydown =(event)=>{
-    if(event.keyCode == 8){
-      viewer.clipper.deletePlane();
+    if(event.key == 'Backspace'){
+      viewer.clipper.deleteAllPlanes();
     }
   };
 };
+
+
+
+function getIfcClass(ifcProject){
+  let typeArray =[];
+  return getIfcClass_base(ifcProject,typeArray);
+}
+function getIfcClass_base(ifcProject,typeArray){
+  const children = ifcProject.children;
+  if (children.length===0){
+    typeArray.push(ifcProject.type);
+  }
+  else {
+    for (const obj of children){
+      getIfcClass_base(obj,typeArray);
+    }
+  }
+  return typeArray;
+}
+
+function test(ifcProject, modelID){
+  const ifcClass=getIfcClass(ifcProject);
+  let uniqueClasses = [...new Set(ifcClass)];
+  
+  console.log(JSON.stringify(uniqueClasses));
+  const classessGUI = document.getElementById("ifc-class-menu-root");
+  for (const ifcClass of uniqueClasses){
+    createClassEntry(ifcClass,classessGUI, modelID);
+  }
+}
+
+function createClassEntry(key, classessGUI, modelID) {
+  const propContainer = document.createElement("div");
+  propContainer.classList.add("ifc-classes-item");
+
+  const keyElement = document.createElement("div");
+  keyElement.textContent = key;
+  propContainer.appendChild(keyElement);
+  
+  const divSvg=document.createElement("button");
+  divSvg.classList.add("btn-ifcClasses");
+  const iconSvg=document.createElementNS('http://www.w3.org/2000/svg','svg');
+  iconSvg.setAttribute('clip-rule', 'evenodd');
+  iconSvg.setAttribute('fill-rule', 'evenodd');
+  iconSvg.setAttribute('stroke-linejoin', 'round');
+  iconSvg.setAttribute('stroke-miterlimit', '2');
+  iconSvg.setAttribute('viewBox', '0 0 24 24');
+  iconSvg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  const pathSvg = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'path'
+  );
+  pathSvg.setAttribute('d', "m17.5 11c2.484 0 4.5 2.016 4.5 4.5s-2.016 4.5-4.5 4.5-4.5-2.016-4.5-4.5 2.016-4.5 4.5-4.5zm-5.346 6.999c-.052.001-.104.001-.156.001-4.078 0-7.742-3.093-9.854-6.483-.096-.159-.144-.338-.144-.517s.049-.358.145-.517c2.111-3.39 5.775-6.483 9.853-6.483 4.143 0 7.796 3.09 9.864 6.493.092.156.138.332.138.507s-.046.351-.138.507l-.008.013c-1.079-1.18-2.631-1.92-4.354-1.92-.58 0-1.141.084-1.671.24-.498-1.643-2.025-2.84-3.829-2.84-2.208 0-4 1.792-4 4 0 2.08 1.591 3.792 3.622 3.982-.014.171-.022.343-.022.518 0 .893.199 1.74.554 2.499zm3.071-2.023 1.442 1.285c.095.085.215.127.333.127.136 0 .271-.055.37-.162l2.441-2.669c.088-.096.131-.217.131-.336 0-.274-.221-.499-.5-.499-.136 0-.271.055-.37.162l-2.108 2.304-1.073-.956c-.096-.085-.214-.127-.333-.127-.277 0-.5.224-.5.499 0 .137.056.273.167.372zm-3.277-2.477c-1.356-.027-2.448-1.136-2.448-2.499 0-1.38 1.12-2.5 2.5-2.5 1.193 0 2.192.837 2.44 1.955-1.143.696-2.031 1.768-2.492 3.044z");
+  pathSvg.setAttribute('fill-rule','nonzero');
+  iconSvg.appendChild(pathSvg);
+  //propContainer.appendChild(iconSvg);
+  divSvg.appendChild(iconSvg);
+  propContainer.appendChild(divSvg);
+
+  classessGUI.appendChild(propContainer);
+  propContainer.onclick=async()=>{
+    const txt=divSvg.parentElement.childNodes[0].textContent;
+    console.log(txt);
+    const filteredIDs = await getObjects(modelID,txt);
+    await viewer.IFC.selector.pickIfcItemsByID(modelID,filteredIDs,false,true);
+  };
+}
+
+async function getObjects(modelID, text){
+  const webifcTypes=viewer.IFC.loader.ifcManager.typesMap;
+  const key= Object.keys(webifcTypes).find(key => webifcTypes[key] === text);
+  const keyNumber=parseInt(key);
+  console.log(key + "KeyNumber:"+keyNumber);
+  const filteredElements= await viewer.IFC.loader.ifcManager.getAllItemsOfType(modelID,keyNumber,true);
+  let filteredIDs=[];
+  for (const element of filteredElements){
+    filteredIDs.push(element.expressID);
+  }
+  //console.log(JSON.stringify(filteredElements));
+  return filteredIDs;
+}
 
 //PROPERTIES (FOR EVERY PROP TYPE)
 const propsGUI = document.getElementById("ifc-property-menu-root");
@@ -113155,15 +113286,6 @@ for (let i = 0; i < toggler.length; i++) {
 
 // Spatial tree menu
 
-/*function createTreeMenu(ifcProject) {
-    const root = document.getElementById("tree-root");
-    removeAllChildren(root);
-    const ifcProjectNode = createNestedChild(root, ifcProject);
-    ifcProject.children.forEach(child => {
-        constructTreeMenuNode(ifcProjectNode, child);
-    })
-}*/
-
 async function createTreeMenu(ifcProject,modelID) {
   const root = document.getElementById("tree-root");
   removeAllChildren(root);
@@ -113181,11 +113303,7 @@ async function renameTree(ifcProject, modelID){
     const valContent=val.textContent.toString();
     if (valContent.includes("IFCPROJECT") || valContent.includes("IFCSITE") || valContent.includes("IFCBUILDING") || valContent.includes("IFCBUILDINGSTOREY")){
       filteredUl.push(val);
-    }  }  /*const nestedUl_array=Array.from(nestedUl);
-  nestedUl_array.find(function(ul){
-    ul.textContent.includes("IFCPROJECT") || ul.textContent.includes("IFCSITE") || ul.textContent.includes("IFCBUILDING") || ul.textContent.includes("IFCBUILDINGSTOREY")
-  })*/
-  const project = filteredUl[0];
+    }  }  const project = filteredUl[0];
   project.textContent=await getProjectName(ifcProject,modelID);
   const site =filteredUl[1];
   site.textContent=await getSiteName(ifcProject,modelID);
@@ -113290,7 +113408,6 @@ async function setUpMultiThreading() {
   // These paths depend on how you structure your project
   await manager.useWebWorkers(true, '../IFCWorker.js');
 }
-
 
 
 /*
